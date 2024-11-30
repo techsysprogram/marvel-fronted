ea43c9ca13a104695ce1697631fa67b6f50e64db
Techsysprogram <techsysprogram@gmail.com>
2024-11-30 02:54:53 +0100

    correction comics

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -2,45 +2,33 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import ComicCard from "../components/ComicCard";
 import Modal from "../components/Modal";
-import CharacterCard from "../components/CharacterCard";
-import Loader from "../components/Loader"; // Import du composant Loader
-import Notification from "../components/Notification"; // Import du composant Notification
+import Loader from "../components/Loader"; // Import du Loader
+import Notification from "../components/Notification"; // Import du Notification
 
-const Favorites = () => {
-  const [activeTab, setActiveTab] = useState("characters");
-  const [favoritesCharacters, setFavoritesCharacters] = useState([]);
-  const [favoritesComics, setFavoritesComics] = useState([]);
+const Comics = ({ search, limit, page, setTotalCount }) => {
+  const [comics, setComics] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedComic, setSelectedComic] = useState(null);
 
-  const fetchFavorites = async () => {
+  const fetchComics = async () => {
     setLoading(true);
     try {
+      const skip = page;
       const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+      const url = `${baseURL}/comics?limit=${limit}&page=${skip}${
+        search ? `&title=${search}` : ""
+      }`;
 
-      const favoritesCharactersIds =
-        JSON.parse(localStorage.getItem("favorites-characters")) || [];
-      const favoritesComicsIds =
-        JSON.parse(localStorage.getItem("favorites-comics")) || [];
+      const response = await axios.get(url);
 
-      const charactersPromises = favoritesCharactersIds.map((id) =>
-        axios.get(`${baseURL}/character/${id}`)
-      );
-      const comicsPromises = favoritesComicsIds.map((id) =>
-        axios.get(`${baseURL}/comic/${id}`)
-      );
+      setComics(response.data.results);
+      setTotalCount(response.data.count);
 
-      const charactersResponses = await Promise.all(charactersPromises);
-      const comicsResponses = await Promise.all(comicsPromises);
-
-      setFavoritesCharacters(charactersResponses.map((res) => res.data));
-      setFavoritesComics(comicsResponses.map((res) => res.data));
-
+      setError(null); // Réinitialise les erreurs si la requête réussit
       setLoading(false);
-      setError(null); // Réinitialise l'erreur après un succès
     } catch (err) {
       setError(err.message);
       setLoading(false);
@@ -48,8 +36,8 @@ const Favorites = () => {
   };
 
   useEffect(() => {
-    fetchFavorites();
-  }, []);
+    fetchComics();
+  }, [search, limit, page]);
 
   const openModal = (comic) => {
     setSelectedComic(comic);
@@ -66,11 +54,10 @@ const Favorites = () => {
   };
 
   return (
-    <div className="favorites-container">
-      {/* Affichage du Loader */}
-      {loading && <Loader />}
+    <div className="details-container">
+      {loading && <Loader />} {/* Affiche le Loader si loading est true */}
 
-      {/* Notification en cas d'erreur */}
+      {/* Affiche la notification en cas d'erreur */}
       {error && (
         <Notification
           message={`Erreur : ${error}`}
@@ -79,54 +66,15 @@ const Favorites = () => {
         />
       )}
 
-      {/* Contenu principal */}
-      {!loading && !error && (
-        <>
-          <div className="tabs">
-            <button
-              className={activeTab === "characters" ? "active" : ""}
-              onClick={() => setActiveTab("characters")}
-            >
-              Mes personnages préférés
-            </button>
-            <button
-              className={activeTab === "comics" ? "active" : ""}
-              onClick={() => setActiveTab("comics")}
-            >
-              Mes comics préférés
-            </button>
-          </div>
+      <div className="groupe-cards">
+        {comics.map((comic) => (
+          <ComicCard key={comic._id} comic={comic} onClick={openModal} />
+        ))}
+      </div>
 
-          {activeTab === "characters" && (
-            <div className="groupe-cards">
-              {favoritesCharacters.length > 0 ? (
-                favoritesCharacters.map((character) => (
-                  <CharacterCard key={character._id} character={character} />
-                ))
-              ) : (
-                <p>Aucun personnage favori.</p>
-              )}
-            </div>
-          )}
-
-          {activeTab === "comics" && (
-            <div className="groupe-cards">
-              {favoritesComics.length > 0 ? (
-                favoritesComics.map((comic) => (
-                  <ComicCard key={comic._id} comic={comic} onClick={openModal} />
-                ))
-              ) : (
-                <p>Aucun comic favori.</p>
-              )}
-            </div>
-          )}
-        </>
-      )}
-
-      {/* Modal */}
       <Modal isOpen={isModalOpen} onClose={closeModal} comic={selectedComic} />
     </div>
   );
 };
 
-export default Favorites;
+export default Comics;
